5be18bedaaee782157f65ff05db6af6dc87745d2
juanhposso <juan.hernandez0793@gmail.com>
2024-04-18 07:42:18 -0500

    setting up the the databases and routes

---
+++ data.js
@@ -0,0 +1,4 @@
+export default {
+	message:
+		"MSH|^~&|SendingApp|SendingFac|ReceivingApp|ReceivingFac|20170101120000||SIU^S12|MSGID12345678901234567890|P|2.5|\rPID|1|123456789|123456789|Doe^John^||19700101|M||C|123 Main St.^^Anytown^WA^12345^USA|||||||123-456-7890|\rPV1|1|O|Room123^1^2^3||||123456^Smith^John|||SUR|||||A0|\rSCH|12345^AMB^|45678^RAD^|O|||||||1^Once^^^^^^^4&5| ^^^^^^^^^^^RADIOLOGY||||||F|||||||A||^^^20170101|^^^20170102|15^Min|\rZDS|1|123456789|20170101120000|20170101121500",
+};
